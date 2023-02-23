2d22dcd522a4ba6a437a81b710e5640cdbd82385
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-02-23 13:14:25 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -3,5 +3,5 @@
  */
 basic.forever(function () {
     servos.P0.setAngle(input.acceleration(Dimension.X))
-    servos.P1.setAngle(input.acceleration(Dimension.Z))
+    servos.P1.setAngle(input.acceleration(Dimension.Y))
 })
